67d1e0d0956b891c3c2850d183bde5ac45281e45
ningli <liniaa@163.com>
2021-10-15 10:20:37 +0800

    update

---
+++ toVuex/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  modules: [
+  //   {
+  //   name: 'coframe',
+  //   swaggerUrl: 'http://petstore.swagger.io/v2/swagger.json',
+  // } swaggerUrl: 'http://lininn.cn/json/1.json',
+      {
+    name: 'eorder',
+   // swaggerUrl: 'http://lininn.cn/json/4.json',
+    //swaggerUrl: 'http://172.31.163.181:8080/v2/api-docs'
+    swaggerUrl:"http://172.30.10.122:8099/v2/api-docs"
+  } 
+  ]
+}
